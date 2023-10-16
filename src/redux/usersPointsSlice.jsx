73ef50f43595d03b0efe491ef85cf81a05209385
Avishag Basel <Avishagbasel@gmail.com>
2023-10-16 16:50:56 +0300

    BP fixes

--- src/redux/usersPointsSlice.jsx
+++ src/redux/usersPointsSlice.jsx
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   usersList: {},
-  currentUser: undefined,
+  currentUser: null,
 };
 
 const counterSlice = createSlice({
--- src/welcome/Welcome.jsx
+++ src/welcome/Welcome.jsx
@@ -8,14 +8,24 @@ import { TextField } from "@mui/material";
 const Welcome = () => {
   const usersList = useSelector((state) => state.usersList);
   const [submittingUserName, setSubmittingUserName] = useState("");
-  const [isDuplicate, setIsDuplicate] = useState(false);
-  let [isSubmitted, setIsSubmitted] = useState(false);
+  let [isValidSubmitted, setIsValidSubmitted] = useState(null);
   const dispatch = useDispatch();
-  const canStartQuiz = !isDuplicate && isSubmitted && submittingUserName;
 
-  function checkIfDuplicate() {
-    return usersList[submittingUserName] !== undefined;
+  function isDuplicateUserName() {
+    return usersList[submittingUserName] === undefined;
   }
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (isDuplicateUserName()) {
+      setIsValidSubmitted(true);
+      dispatch(setUserName(submittingUserName));
+    } else {
+      setIsValidSubmitted(false);
+    }
+  };
+
   return (
     <div>
       <QuizHeader variant="h1">Lets Quizing!</QuizHeader>
@@ -25,45 +35,38 @@ const Welcome = () => {
           justifyContent: "center",
         }}
       >
-        {!canStartQuiz && (
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              setIsSubmitted(true);
-              if (!checkIfDuplicate() && submittingUserName) {
-                setIsDuplicate(false);
-                dispatch(setUserName(submittingUserName));
-              } else {
-                setIsDuplicate(true);
-              }
-            }}
-          >
+        {!isValidSubmitted ? (
+          <form onSubmit={handleFormSubmit}>
             <TextField
               label="User Name"
               value={submittingUserName}
               placeholder="Enter your name"
               onChange={(e) => setSubmittingUserName(e.target.value)}
               variant="outlined"
-              error={isSubmitted}
+              error={isValidSubmitted === false}
               helperText={
-                isDuplicate
+                isValidSubmitted === false
                   ? "This name is already taken. Choose another unique one."
                   : ""
               }
             />
-            <SubmitButton type="submit" variant="contained" color="primary">
+            <SubmitButton
+              type="submit"
+              variant="contained"
+              color="primary"
+              disabled={submittingUserName.trim().length === 0}
+            >
               Submit
             </SubmitButton>
           </form>
-        )}
-        {canStartQuiz && (
+        ) : (
           <StartQuizButton
             component={Link}
             to="/quiz"
             variant="contained"
             color="success"
           >
-            Lets Start {submittingUserName} !
+            Let's Start {submittingUserName} !
           </StartQuizButton>
         )}
       </div>
